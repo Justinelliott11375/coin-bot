ac07bea10848d0efc2638b84692d7a45dde554f8
Justin Elliott <Justinelliott11375@gmail.com>
2020-12-10 16:38:19 -0600

    git ignore file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import cbpro
+
+public_client = cbpro.PublicClient()
+products = public_client.get_products()
+
 print('hello world')
+
+# for foo in products:
+#  print(foo['id'])
+
+print(public_client.get_product_order_book('XLM-USD'))
